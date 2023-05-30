7b0ed1eee255821018bdbabfba50cd561cb3aa62
Seth Addo <59029978+seths10@users.noreply.github.com>
2023-05-30 03:20:43 +0000

    Add files via upload

---
+++ webhook.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+const payloads = [];
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  const payload = req.body;
+  payloads.push(payload);
+  console.log('Received payload:', payload);
+
+  // Send a response back to The Things Stack
+  res.status(200).send('Webhook received successfully!');
+});
+
+app.get('/payloads', (req, res) => {
+  res.status(200).json(payloads);
+});
+
+app.listen(port, () => {
+  console.log(`Webhook server is running on port ${port}`);
+});
